1980b5a33edf2ae93b37c1a229fbf15a1f26d8b5
Gabriel <gabriel.galiano13@gmail.com>
2026-05-20 10:36:58 -0300

    fix: apanha y apanha

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../src/generated/prisma/client.js';
+import { PrismaClient } from '../src/generated/prisma-client/client';
 import bcrypt from 'bcryptjs';
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });

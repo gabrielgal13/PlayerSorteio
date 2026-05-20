3aafd5e51d3547325882f224c340eeef5ccad2de
Gabriel <gabriel.galiano13@gmail.com>
2026-05-20 10:23:58 -0300

    fix: prisma last time

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma-client/clien';
+import { PrismaClient } from '../generated/prisma-client/client';
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
